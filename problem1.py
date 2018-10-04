d642081f832298251698414ce27ce185580cc8eb
laurenmitchell <lauren.a.mitchell@colorado.edu>
2018-10-04 10:43:43 -0400

    adding problem 1 and problem 2 files

---
+++ problem1.py
@@ -0,0 +1,12 @@
+#homework1
+#Due 10/10/18
+
+# Problem 1
+#Write a Python program to count the number
+#of even and odd numbers from a series of numbers.
+#Be sure to print out what numbers are even and what numbers are odd.
+#(Hint use a for loop)
+
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) # Declaring the tuple
+countOdd =
+countEven = 
---
+++ problem2.py
@@ -0,0 +1,13 @@
+#homework1
+#Due 10/10/18
+
+# Problem 2
+#Write a Python program to guess a number between 1 to 9.
+# User is prompted to enter a guess. If the user guesses wrong then
+#the prompt appears again until the guess is correct, on successful guess,
+#user will get a "Well guessed!" message, and the program will exit.
+#(Hint use a while loop)
+#Don't forget to import random
+
+targetNum =
+guessNum = input()
