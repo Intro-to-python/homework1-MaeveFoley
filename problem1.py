05ba9ccd4171c9229250f5b48c3026aaea2351ba
Maeve <mfoley@cshnyc.org>
2018-10-15 19:43:17 -0400

    countevenodd

--- problem1.py
+++ problem1.py
@@ -7,6 +7,19 @@
 #Be sure to print out what numbers are even and what numbers are odd.
 #(Hint use a for loop)
 
-numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) # Declaring the tuple
-countOdd =
-countEven = 
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+countOdd = 0
+countEven = 0
+for x in numbers:
+  if x % 2 == 0:
+    countEven+=1
+  else:
+    countOdd+=1
+print("There are ", countEven, "even numbers")
+print("There are", countOdd, "odd numbers")
+
+for x in numbers:
+  if x % 2 == 0:
+    print(x, "is even")
+  else:
+    print(x, "is odd")
